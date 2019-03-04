20fd56d9528735902ace5eac6f4fa69265342a0e
Carles Sala <carles@pythiac.com>
2019-03-04 16:35:53 +0100

    remove unused import

--- orion/model.py
+++ orion/model.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from mongoengine import Document, EmbeddedDocument, fields
+from mongoengine import Document, fields
 
 from orion.utils import remove_dots, restore_dots
 
