ee606db850ed69faa395e334011a048e2a0e0cc2
Carles Sala <carles@pythiac.com>
2020-04-23 18:53:17 +0200

    Bump version: 0.1.0 → 0.1.1.dev0

--- setup.py
+++ setup.py
@@ -110,6 +110,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/D3-AI/Orion',
-    version='0.1.0',
+    version='0.1.1.dev0',
     zip_safe=False,
 )
