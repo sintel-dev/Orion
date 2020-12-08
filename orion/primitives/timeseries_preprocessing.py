70efb9fcfcab94fc8d0a775f51936ae69062da57
sarahmish <sarahalnegheimish@gmail.com>
2020-12-08 10:09:06 -0500

    update missing value imputation

--- orion/primitives/timeseries_preprocessing.py
+++ orion/primitives/timeseries_preprocessing.py
@@ -1,70 +1,20 @@
-import numpy as np
+import pandas as pd
 
 
-def ffill(X):
-    """Fill NaN values with last known value.
-
-    This function fills the missing values of the input sequence with the last
-    known value. If there are contigous NaN values, they will all be filled with
-    the same last known value.
-
-    Args:
-        X (ndarray):
-            Array of input sequence.
-
-        Returns:
-        ndarray:
-            Array of input sequence with imputed values.
-    """
-    if X.ndim == 1:
-        X = X.reshape(1, -1)
-
-    mask = np.isnan(X)
-    idx = np.where(~mask, np.arange(mask.shape[1]), 0)
-    np.maximum.accumulate(idx, axis=1, out=idx)
-    X[mask] = X[np.nonzero(mask)[0], idx[mask]]
-
-    return X
-
-
-def bfill(X):
-    """Fill NaN values with next known value.
-
-    This function fills the missing values of the input sequence with the upcoming
-    known value. If there are contigous NaN values, they will all be filled with
-    the same next known value.
-
-    Args:
-        X (ndarray):
-            Array of input sequence.
-
-    Returns:
-        ndarray:
-            Array of input sequence with imputed values.
-    """
-    if X.ndim == 1:
-        X = X.reshape(1, -1)
-
-    mask = np.isnan(X)
-    idx = np.where(~mask, np.arange(mask.shape[1]), mask.shape[1] - 1)
-    idx = np.minimum.accumulate(idx[:, ::-1], axis=1)[:, ::-1]
-    X[mask] = X[np.nonzero(mask)[0], idx[mask]]
-
-    return X
-
-
-def fillna(X, method="ffill"):
+def fillna(X, method=["ffill"], **kwargs):
     """Impute missing values.
 
-    This function fills the missing values of the input sequence with the next or
+    This function fills the missing values of the input sequence with the next/
     previous known value. If there are contigous NaN values, they will all be
-    filled with the same next or previous known value.
+    filled with the same next/previous known value.
 
     Args:
         X (ndarray):
             Array of input sequence.
         method (str or list):
-            Optional. String describing whether to use forward or backward fill.
+            Optional. String describing whether to use forward or backward fill. pad / ffill:
+            propagate last valid observation forward to next valid. backfill / bfill: use next
+            valid observation to fill gap.
 
     Returns:
         ndarray:
@@ -73,20 +23,9 @@ def fillna(X, method="ffill"):
     if isinstance(method, str):
         method = [method]
 
-    dim = X.ndim
-    X_ = X.copy()
-
-    if dim == 1:
-        X_ = X_.reshape(1, -1)
+    X_ = pd.Series(X)
 
     for fill in method:
-        if fill == "ffill":
-            X_ = ffill(X_)
+        X_ = X_.fillna(method=fill, **kwargs)
 
-        elif fill == "bfill":
-            X_ = bfill(X_)
-
-    if dim == 1:
-        X_ = X_.reshape(-1,)
-
-    return X_
+    return X_.values
--- tests/primitives/test_timeseries_preprocessing.py
+++ tests/primitives/test_timeseries_preprocessing.py
@@ -1,7 +1,7 @@
 import numpy as np
 from numpy.testing import assert_array_equal
 
-from orion.primitives.timeseries_preprocessing import bfill, ffill, fillna
+from orion.primitives.timeseries_preprocessing import fillna
 
 
 def single_dimensional():
@@ -16,46 +16,11 @@ def multidimensional():
                      [4, 7, np.nan]])
 
 
-def test_ffill():
+def test_fillna_ffill():
     X = single_dimensional()
-    expected_return = np.array([[0, 3, 3, 4, 3, 2, 2, 2, 2]])
+    expected_return = np.array([0, 3, 3, 4, 3, 2, 2, 2, 2])
 
-    returned = ffill(X)
-
-    assert_array_equal(returned, expected_return)
-
-
-def test_ffill_multidimensional():
-    X = multidimensional()
-    expected_return = np.array([[0, 3, 5],
-                                [np.nan, 2, 4],
-                                [5, 5, 6],
-                                [9, 9, 3],
-                                [4, 7, 7]])
-
-    returned = ffill(X)
-
-    assert_array_equal(returned, expected_return)
-
-
-def test_bfill():
-    X = single_dimensional()
-    expected_return = np.array([[0, 3, 4, 4, 3, 2, 2, np.nan, np.nan]])
-
-    returned = bfill(X)
-
-    assert_array_equal(returned, expected_return)
-
-
-def test_bfill_multidimensional():
-    X = multidimensional()
-    expected_return = np.array([[0, 3, 5],
-                                [2, 2, 4],
-                                [5, 6, 6],
-                                [9, 3, 3],
-                                [4, 7, np.nan]])
-
-    returned = bfill(X)
+    returned = fillna(X, method='ffill')
 
     assert_array_equal(returned, expected_return)
 
