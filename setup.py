fe0865960c7af5bfd8a0ddc182eab7f8f7ad27b3
sarahmish <sarahalnegheimish@gmail.com>
2021-05-04 02:39:54 -0400

    Bump version: 0.1.7.dev0 → 0.1.7.dev1

--- setup.py
+++ setup.py
@@ -120,6 +120,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/D3-AI/Orion',
-    version='0.1.7.dev0',
+    version='0.1.7.dev1',
     zip_safe=False,
 )
