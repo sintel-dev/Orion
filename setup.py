2ab8d5c7dae55e38f430168a414a71d14d590e49
sarahmish <sarahalnegheimish@gmail.com>
2022-04-26 22:05:51 -0400

    Bump version: 0.3.1 → 0.3.2.dev0

--- setup.py
+++ setup.py
@@ -116,6 +116,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sintel-dev/Orion',
-    version='0.3.1',
+    version='0.3.2.dev0',
     zip_safe=False,
 )
