553daf661616fff12acf50255942b963c68ae385
sarahmish <sarahalnegheimish@gmail.com>
2022-09-16 17:49:22 -0400

    reduce runs

--- tasks.py
+++ tasks.py
@@ -72,7 +72,7 @@ def tutorials(c):
             continue
         if '.ipynb_checkpoints' not in ipynb_file:
             c.run((
-                'jupyter nbconvert --execute --ExecutePreprocessor.timeout=5400 '
+                'jupyter nbconvert --execute --ExecutePreprocessor.timeout=3600 '
                 f'--to=html --stdout {ipynb_file}'
             ), hide='out')
 
