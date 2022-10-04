e58e8db788a9f27a145036aa358bdb3d348ab73b
sarahmish <sarahalnegheimish@gmail.com>
2022-10-04 14:26:36 -0400

    add python 3.8

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ setup(
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
     description="Orion is a machine learning library built for unsupervised time series anomaly detection.",
     entry_points={
