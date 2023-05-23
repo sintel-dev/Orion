4d717501cbf8a0d7097929be622bd69fa8250823
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2023-05-23 15:40:20 -0400

    Bump version: 0.5.0 → 0.5.1.dev0

--- setup.py
+++ setup.py
@@ -126,6 +126,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/sintel-dev/Orion',
-    version='0.5.0',
+    version='0.5.1.dev0',
     zip_safe=False,
 )
