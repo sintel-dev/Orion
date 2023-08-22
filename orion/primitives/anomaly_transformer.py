6e6c4840481e19a569e5f8c1ae044554e8e34a8a
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2023-08-21 22:12:23 -0400

    fix variable names

--- orion/primitives/anomaly_transformer.py
+++ orion/primitives/anomaly_transformer.py
@@ -2,13 +2,21 @@
 
 This primitive is an pytorch implementation of "Anomaly Transformer: 
 Time Series Anomaly Detection with Association Discrepancy"
-https://openreview.net/pdf?id=LzQQ89U1qm_
+https://arxiv.org/pdf/2110.02642.pdf
 
 This is a modified version of the original code, which can be found
 at https://github.com/thuml/Anomaly-Transformer/tree/main
+
+win_size -> window_size
+n_heads -> num_heads
+d_model -> d_model
+e_layers -> num_layers
+enc_in -> input_size
+
 """
 import os
 import math
+import logging
 
 import numpy as np
 import torch
@@ -16,7 +24,9 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils import weight_norm
 from torch.utils.data import DataLoader
+from mlstars.utils import import_object
 
+LOGGER = logging.getLogger(__name__)
 
 class Signal(object):
     """Data object.
@@ -44,15 +54,15 @@ class Signal(object):
         return np.float32(self.data[start:end])
 
 
-class PositionalEmbedding(nn.Module):
+class PositionalEncoding(nn.Module):
     def __init__(self, d_model, max_len=5000):
-        super(PositionalEmbedding, self).__init__()
-        # Compute the positional encodings once in log space.
-        pe = torch.zeros(max_len, d_model).float()
+        super(PositionalEncoding, self).__init__()
+
+        pe = torch.zeros(max_len, d_model)
         pe.require_grad = False
 
-        position = torch.arange(0, max_len).float().unsqueeze(1)
-        div_term = (torch.arange(0, d_model, 2).float() * -(math.log(10000.0) / d_model)).exp()
+        position = torch.arange(0, max_len).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2) * -(math.log(10000.0) / d_model))
 
         pe[:, 0::2] = torch.sin(position * div_term)
         pe[:, 1::2] = torch.cos(position * div_term)
@@ -65,25 +75,27 @@ class PositionalEmbedding(nn.Module):
 
 
 class TokenEmbedding(nn.Module):
-    def __init__(self, c_in, d_model):
+    def __init__(self, input_size, d_model):
         super(TokenEmbedding, self).__init__()
+
         padding = 1 if torch.__version__ >= '1.5.0' else 2
-        self.tokenConv = nn.Conv1d(in_channels=c_in, out_channels=d_model,
-                                   kernel_size=3, padding=padding, padding_mode='circular', bias=False)
+        self.tokenConv = nn.Conv1d(in_channels=input_size, out_channels=d_model,
+                                   kernel_size=3, padding=padding, padding_mode='circular', 
+                                   bias=False)
+
         for m in self.modules():
             if isinstance(m, nn.Conv1d):
                 nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='leaky_relu')
 
     def forward(self, x):
-        x = self.tokenConv(x.permute(0, 2, 1)).transpose(1, 2)
-        return x
+        return self.tokenConv(x.permute(0, 2, 1)).transpose(1, 2)
 
 
 class DataEmbedding(nn.Module):
-    def __init__(self, c_in, d_model, dropout=0.0):
+    def __init__(self, input_size, d_model, dropout=0.0):
         super(DataEmbedding, self).__init__()
 
-        self.value_embedding = TokenEmbedding(c_in=c_in, d_model=d_model)
+        self.value_embedding = TokenEmbedding(input_size=input_size, d_model=d_model)
         self.position_embedding = PositionalEmbedding(d_model=d_model)
 
         self.dropout = nn.Dropout(p=dropout)
@@ -97,7 +109,8 @@ class TriangularCausalMask():
     def __init__(self, B, L, device="cpu"):
         mask_shape = [B, 1, L, L]
         with torch.no_grad():
-            self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), diagonal=1).to(device)
+            self._mask = torch.triu(torch.ones(mask_shape, dtype=torch.bool), 
+                diagonal=1).to(device)
 
     @property
     def mask(self):
@@ -105,13 +118,16 @@ class TriangularCausalMask():
 
 
 class AnomalyAttention(nn.Module):
-    def __init__(self, window_size, mask_flag=True, scale=None, attention_dropout=0.0, output_attention=False):
+    def __init__(self, window_size, mask_flag=True, scale=None, attention_dropout=0.0, 
+                 output_attention=False, device="cpu"):
         super(AnomalyAttention, self).__init__()
+
         self.scale = scale
         self.mask_flag = mask_flag
         self.output_attention = output_attention
+        self.device = device
         self.dropout = nn.Dropout(attention_dropout)
-        self.distances = torch.zeros((window_size, window_size)).cuda()
+        self.distances = torch.zeros((window_size, window_size)).to(device)
         for i in range(window_size):
             for j in range(window_size):
                 self.distances[i][j] = abs(i - j)
@@ -125,15 +141,18 @@ class AnomalyAttention(nn.Module):
         if self.mask_flag:
             if attn_mask is None:
                 attn_mask = TriangularCausalMask(B, L, device=queries.device)
+            
             scores.masked_fill_(attn_mask.mask, -np.inf)
+        
         attn = scale * scores
-
         sigma = sigma.transpose(1, 2)  # B L H ->  B H L
         window_size = attn.shape[-1]
         sigma = torch.sigmoid(sigma * 5) + 1e-5
         sigma = torch.pow(3, sigma) - 1
         sigma = sigma.unsqueeze(-1).repeat(1, 1, 1, window_size)  # B H L L
-        prior = self.distances.unsqueeze(0).unsqueeze(0).repeat(sigma.shape[0], sigma.shape[1], 1, 1).cuda()
+        prior = self.distances.unsqueeze(0).unsqueeze(0).repeat(
+            sigma.shape[0], sigma.shape[1], 1, 1).to(self.device)
+        
         prior = 1.0 / (math.sqrt(2 * math.pi) * sigma) * torch.exp(-prior ** 2 / 2 / (sigma ** 2))
 
         series = self.dropout(torch.softmax(attn, dim=-1))
@@ -146,26 +165,25 @@ class AnomalyAttention(nn.Module):
 
 
 class AttentionLayer(nn.Module):
-    def __init__(self, attention, d_model, n_heads, d_keys=None,
-                 d_values=None):
+    def __init__(self, attention, d_model, num_heads, d_keys=None, d_values=None):
         super(AttentionLayer, self).__init__()
 
-        d_keys = d_keys or (d_model // n_heads)
-        d_values = d_values or (d_model // n_heads)
+        d_keys = d_keys or (d_model // num_heads)
+        d_values = d_values or (d_model // num_heads)
         self.norm = nn.LayerNorm(d_model)
         self.inner_attention = attention
-        self.query_projection = nn.Linear(d_model, d_keys * n_heads)
-        self.key_projection = nn.Linear(d_model, d_keys * n_heads)
-        self.value_projection = nn.Linear(d_model, d_values * n_heads)
-        self.sigma_projection = nn.Linear(d_model, n_heads)
-        self.out_projection = nn.Linear(d_values * n_heads, d_model)
+        self.query_projection = nn.Linear(d_model, d_keys * num_heads)
+        self.key_projection = nn.Linear(d_model, d_keys * num_heads)
+        self.value_projection = nn.Linear(d_model, d_values * num_heads)
+        self.sigma_projection = nn.Linear(d_model, num_heads)
+        self.out_projection = nn.Linear(d_values * num_heads, d_model)
 
-        self.n_heads = n_heads
+        self.num_heads = num_heads
 
     def forward(self, queries, keys, values, attn_mask):
         B, L, _ = queries.shape
         _, S, _ = keys.shape
-        H = self.n_heads
+        H = self.num_heads
         x = queries
         queries = self.query_projection(queries).view(B, L, H, -1)
         keys = self.key_projection(keys).view(B, S, H, -1)
@@ -185,12 +203,13 @@ class AttentionLayer(nn.Module):
 
 
 class EncoderLayer(nn.Module):
-    def __init__(self, attention, d_model, d_ff=None, dropout=0.1, activation="relu"):
+    def __init__(self, attention, d_model, n_hidden=None, dropout=0.1, activation="relu"):
         super(EncoderLayer, self).__init__()
-        d_ff = d_ff or 4 * d_model
+        
+        n_hidden = n_hidden or 4 * d_model
         self.attention = attention
-        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=d_ff, kernel_size=1)
-        self.conv2 = nn.Conv1d(in_channels=d_ff, out_channels=d_model, kernel_size=1)
+        self.conv1 = nn.Conv1d(in_channels=d_model, out_channels=n_hidden, kernel_size=1)
+        self.conv2 = nn.Conv1d(in_channels=n_hidden, out_channels=d_model, kernel_size=1)
         self.norm1 = nn.LayerNorm(d_model)
         self.norm2 = nn.LayerNorm(d_model)
         self.dropout = nn.Dropout(dropout)
@@ -233,31 +252,37 @@ class Encoder(nn.Module):
 
 
 class ATModel(nn.Module):
-    def __init__(self, window_size, enc_in, c_out, d_model=512, n_heads=8, e_layers=3, d_ff=512,
-                 dropout=0.0, activation='gelu', output_attention=True):
+    def __init__(self, window_size, input_size, output_size, d_model=512, num_heads=8, 
+                 num_layers=3, n_hidden=512, dropout=0.0, activation='gelu', 
+                 output_attention=True, attention_dropout=0.0, device="cpu"):
         super(AnomalyTransformer, self).__init__()
         self.output_attention = output_attention
 
-        # Encoding
-        self.embedding = DataEmbedding(enc_in, d_model, dropout)
+        # Embedding
+        self.embedding = DataEmbedding(input_size, d_model, dropout)
 
         # Encoder
         self.encoder = Encoder(
             [
                 EncoderLayer(
                     AttentionLayer(
-                        AnomalyAttention(window_size, False, attention_dropout=dropout, output_attention=output_attention),
-                        d_model, n_heads),
-                    d_model,
-                    d_ff,
+                        AnomalyAttention(
+                            window_size=window_size, mask_flag=False, scale=None, 
+                            attention_dropout=attention_dropout, output_attention=output_attention,
+                            device=device
+                        ),
+                        d_model=d_model, num_heads=num_heads
+                    ),
+                    d_model=d_model,
+                    n_hidden=n_hidden,
                     dropout=dropout,
                     activation=activation
-                ) for l in range(e_layers)
+                ) for l in range(num_layers)
             ],
             norm_layer=torch.nn.LayerNorm(d_model)
         )
 
-        self.projection = nn.Linear(d_model, c_out, bias=True)
+        self.projection = nn.Linear(d_model, output_size, bias=True)
 
     def forward(self, x):
         enc_out = self.embedding(x)
@@ -284,36 +309,26 @@ class AnomalyTransformer(object):
             The time gap between one sample and another.
         input_size (int):
             Input size for the network.
-        n_blocks (int):
-            Number of blocks to stack in a model (MADE in MAF; Coupling+BN in RealNVP).
-        n_components (int):
-            Number of Gaussian clusters for mixture of gaussians models.
-        hidden_size (int):
-            Hidden layer size for MADE (and each MADE block in an MAF).
+        output_size (int):
+            Output size for the network.
+        d_model (int):
+            Model dimension.
         n_hidden (int):
-            Number of hidden layers in each MADE.
-        batch_norm (bool):
-            Whether to use batch norm or not.
+            Hidden dimension.
         batch_size (int):
             Number of example per epoch.
         dropout (float):
             Dropout value of the network.
-        weight_decay (float):
-            Weight decay rate for Adam.
+        attention_dropout (float):
+            Dropout value for attention.
         epochs (int):
             Number of iterations to train the model.
         learning_rate (float):
             Learning rate for the optimizer.
-        log_interval (int):
-            How often to show loss statistics and save samples.
-        max_iter (int):
-            Maximum number of evaluations to find ``h``.
         verbose (bool):
             Whether to be on verbose mode or not.
         cuda (bool):
             Whether to use GPU or not.
-        seed (int):
-            Random seed to fix in training.
         valid_split (float):
             A float to split data dataframe to validation set. Data needs to contain a label
             column. Use ``target_column`` to change the target column name.
@@ -322,20 +337,68 @@ class AnomalyTransformer(object):
         output_dir (str):
             Path to folder where to save the model.
     """
+    @staticmethod
+    def _kl_loss(p, q):
+        res = p * (torch.log(p + 0.0001) - torch.log(q + 0.0001))
+        return torch.mean(torch.sum(res, dim=-1), dim=1)
 
-    def __init__(self, window_size, step, batch_size, shuffle, cuda, verbose):
+    @staticmethod
+    def _adjust_learning_rate(optimizer, epoch, lr_):
+        lr_adjust = {epoch: lr_ * (0.5 ** ((epoch - 1) // 1))}
+        if epoch in lr_adjust.keys():
+            lr = lr_adjust[epoch]
+            for param_group in optimizer.param_groups:
+                param_group['lr'] = lr
+
+            LOGGER.info(f'Updating learning rate to {lr}')
+
+    def __init__(self, input_size=55, output_size=55, window_size=100, step=1, d_model=512, 
+                 n_hidden=512, num_layers=3, num_heads=8, attention_dropout=0.0, dropout=0.0, 
+                 activation='gelu', output_attention=True, batch_size=32, learning_rate=1e-4, 
+                 epochs=10, shuffle=True, cuda=True, verbose=False):
+
+        self.input_size = input_size
+        self.output_size = output_size
         self.window_size = window_size
         self.step = step
+
+        self.d_model = d_model
+        self.n_hidden = n_hidden
+        self.num_layers = num_layers
+        self.num_heads = num_heads
+        self.dropout = dropout
+        self.activation = activation
+        self.output_attention = output_attention
+        self.attention_dropout = attention_dropout
+        
         self.batch_size = batch_size
+        self.learning_rate = learning_rate
+        self.epochs = epochs
         self.shuffle = shuffle
+        self.cuda = cuda
+        self.verbose = verbose
 
         if cuda and torch.cuda.is_available():
             self.device = "cuda"
 
         # build model
-        self.model = AnomalyTransformer(win_size=self.win_size, enc_in=self.input_c, c_out=self.output_c, e_layers=3)
-        self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+        self.model = ATModel(
+            window_size=self.window_size, 
+            input_size=self.input_size, 
+            output_size=self.output_size, 
+            d_model=self.d_model, 
+            num_heads=self.num_heads, 
+            num_layers=self.num_layers, 
+            n_hidden=self.n_hidden,
+            dropout=self.dropout, 
+            activation=self.activation, 
+            output_attention=self.output_attention,
+            attention_dropout=self.attention_dropout,
+            device=self.device
+        )
 
+        self.optimizer = import_object(optimizer)(self.model.parameters(), lr=self.learning_rate)
+        self.criterion = nn.MSELoss()
         self.model.to(self.device)
 
 
