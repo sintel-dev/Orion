fe7e0a48cf6d68c9775f66d51df73c2425a8d0ff
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2023-09-21 11:44:46 -0400

    fix vae

--- setup.py
+++ setup.py
@@ -18,10 +18,10 @@ except IOError:
 
 install_requires = [
     "tensorflow>=2.0,<2.5;python_version<'3.8'",
-    "tensorflow>=2.2,<2.10;python_version>='3.8'",
+    "tensorflow>=2.2,<2.11;python_version>='3.8'",
     's3fs>=0.2.2,<0.5',
     'mlblocks>=0.6,<0.7',
-    'ml-stars>=0.1.2,<0.2',
+    'ml-stars>=0.1.3.dev0,<0.2',
     'numpy>=1.17.4,<1.21',
     'pandas>=1,<2',
     'scikit-learn>=0.22,<1.2',
