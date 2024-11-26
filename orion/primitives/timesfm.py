ab2375fe32ca3fa5f09461d57183ef72c23b4fe2
Linh Nguyen <linhnk@mit.edu>
2024-11-26 13:49:28 -0500

    hyperparams

--- orion/primitives/timesfm.py
+++ orion/primitives/timesfm.py
@@ -12,35 +12,6 @@ import numpy as np
 import timesfm as tf
 
 
-def rolling_window_sequences(X, window_size, step_size):
-    """Create rolling window sequences out of time series data.
-
-    This function creates an array of sequences by rolling over the input sequence.
-
-    Args:
-        X (ndarray):
-            The sequence to iterate over.
-        window_size (int):
-            Length of window.
-        step_size (int):
-            Indicating the number of steps to move the window forward each round.
-
-    Returns:
-        ndarray, ndarray:
-            * rolling window sequences.
-    """
-    out_X = list()
-
-    start = 0
-    max_start = len(X) - window_size
-    while start < max_start:
-        end = start + window_size
-        out_X.append(X[start:end])
-        start = start + step_size
-
-    return np.asarray(out_X)
-
-
 class TimesFM:
     """TimesFM model for timeseries forecasting.
 
@@ -60,13 +31,11 @@ class TimesFM:
 
     def __init__(self,
                  window_size=256,
-                 step=1,
                  pred_len=1,
                  repo_id="google/timesfm-1.0-200m",
                  freq=0):
 
         self.window_size = window_size
-        self.step_size = step
         self.pred_len = pred_len
         self.freq = freq
 
@@ -76,26 +45,16 @@ class TimesFM:
 
         self.model.load_from_checkpoint(repo_id=repo_id)
 
-    def predict(self, X, index):
+    def predict(self, X):
         """Forecasting timeseries
 
         Args:
             X (ndarray):
                 input timeseries.
-            index (ndarray):
-                timestamps array.
         Return:
-            ndarray, ndarray, ndarray:
-                * forecasted timeseries.
-                * array of truncated ground truth with same size as forecasted timeseries.
-                * array of timestamps with same size as forecasted timeseries.
+            ndarray:
+                forecasted timeseries.
         """
-        X_windows = rolling_window_sequences(X,
-                                             window_size=self.window_size,
-                                             step_size=self.step_size)
-        frequency_input = [self.freq for _ in range(len(X_windows))]
-        y_hat, _ = self.model.forecast(X_windows[:, :, 0], freq=frequency_input)
-        y_hat = y_hat[:, 0]
-        y = X[self.window_size:]
-        index = index[self.window_size:]
-        return y_hat, y, index
+        frequency_input = [self.freq for _ in range(len(X))]
+        y_hat, _ = self.model.forecast(X[:, :, 0], freq=frequency_input)
+        return y_hat[:, 0]
