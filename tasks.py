e31e5bf718cfcb09c86054e358f17f598947706b
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2024-11-26 11:40:47 -0500

    remove pretrained pipelines from tutorial tests

--- tasks.py
+++ tasks.py
@@ -92,14 +92,16 @@ def readme(c):
 
 @task
 def tutorials(c):
+    pipelines = os.listdir(os.path.join('orion', 'pipelines', 'pretrained'))
     for ipynb_file in glob.glob('tutorials/*.ipynb') + glob.glob('tutorials/**/*.ipynb'):
-        if 'OrionDBExplorer' in ipynb_file: # skip db testing
-            continue
-        if '.ipynb_checkpoints' not in ipynb_file:
-            c.run((
-                'jupyter nbconvert --execute --ExecutePreprocessor.timeout=4400 '
-                f'--to=html --stdout {ipynb_file}'
-            ), hide='out')
+        for pipeline in pipelines:
+            if pipeline in ipynb_file: # skip pretrained pipelines
+                continue
+            if '.ipynb_checkpoints' not in ipynb_file:
+                c.run((
+                    'jupyter nbconvert --execute --ExecutePreprocessor.timeout=4400 '
+                    f'--to=html --stdout {ipynb_file}'
+                ), hide='out')
 
 @task
 def pretrained(c):
