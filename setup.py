1c80325ae816b6fceabb776ed182251453d3bc7f
Linh Nguyen <linhnk@mit.edu>
2024-12-17 04:28:23 -0500

    relax jax constraint

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ pretrained_requires = [
 
     #timesfm
     "timesfm[torch]>=1.2.0,<1.5;python_version>='3.11'",
-    "jax>=0.4.20,<0.7;python_version>'3.11'",
+    "jax>=0.4.20,<0.7;python_version>='3.11'",
 
 ]
 
