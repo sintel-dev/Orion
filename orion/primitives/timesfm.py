8374075496a63402c789926e6770c416ca4f871d
Linh Nguyen <linhnk@mit.edu>
2024-12-24 22:41:11 -0500

    support multivaraite

--- orion/primitives/timesfm.py
+++ orion/primitives/timesfm.py
@@ -8,8 +8,8 @@ The model implementation can be found at
 https://github.com/google-research/timesfm?tab=readme-ov-file
 """
 
-import numpy as np
 import timesfm as tf
+import numpy as np
 
 
 class TimesFM:
