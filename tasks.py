4f6564e24b49fb4215c327c159528655c2a3376b
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2025-01-21 19:38:40 -0500

    fix minimum tests

--- tasks.py
+++ tasks.py
@@ -75,7 +75,7 @@ def install_minimum(c):
 def minimum(c):
     install_minimum(c)
     c.run('python -m pip check')
-    c.run('python -m pytest')
+    c.run('python -m pytest ./tests/unit')
 
 
 @task
