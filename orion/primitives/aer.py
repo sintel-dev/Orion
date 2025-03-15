4fa07c31a0335c622c93636a642db6a079f5b408
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2025-03-15 01:39:09 -0400

    adjust layer check

--- orion/primitives/aer.py
+++ orion/primitives/aer.py
@@ -19,7 +19,7 @@ def build_layer(layer: dict, hyperparameters: dict):
     layer_class = import_object(layer['class'])
     layer_kwargs = layer['parameters'].copy()
     # TODO: Upgrade to using tf.keras.layers.Wrapper in mlprimitives.
-    if issubclass(layer_class, tf.keras.layers.Wrapper):
+    if issubclass(layer_class, tf.keras.layers.Layer) and 'layer' in layer_kwargs:
         layer_kwargs['layer'] = build_layer(layer_kwargs['layer'], hyperparameters)
     for key, value in layer_kwargs.items():
         if isinstance(value, str):
